ff35831d7a1486103339089b777ce727d32f7c07
kojing777 <kojingmoktan92@gmail.com>
2025-09-06 21:45:10 +0545

    Cart

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAppContext } from "../context/AppContext";
+import { useAppContext } from "../Context/AppContext";
 import { assets } from "../assets/assets";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
